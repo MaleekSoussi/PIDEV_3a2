e7b44050892743f34182c30d2cc946464de24f1d
Mehdi zaiem <Mehdizaiem@hotmail.fr>
2024-02-18 20:58:51 +0100

    version 1 CRUD

---
+++ src/main/java/controllers/Auction/ItemController.java
@@ -0,0 +1,28 @@
+package controllers.Auction;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ItemController {
+
+    @FXML
+    private ImageView Auctionimage;
+
+    @FXML
+    private Label Auctionname;
+
+    @FXML
+    private TextField bitcoinprice;
+
+    @FXML
+    private TextField pice;
+
+    @FXML
+    void click(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/controllers/Auction/ViewAuctionController.java
@@ -0,0 +1,30 @@
+package controllers.Auction;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+
+public class ViewAuctionController {
+
+    @FXML
+    private Label Auctiionname;
+
+    @FXML
+    private ImageView Auctionoftheday;
+
+    @FXML
+    private VBox chosenFruitCard;
+
+    @FXML
+    private Label fruitNameLable;
+
+    @FXML
+    private GridPane grid;
+
+    @FXML
+    private ScrollPane scroll;
+
+}
