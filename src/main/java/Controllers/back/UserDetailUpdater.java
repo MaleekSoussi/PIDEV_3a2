4344bdbf2a5183fe9975ae4c79b5f32315409679
Hamzachaiebgit <100535374+Hamzachaiebgit@users.noreply.github.com>
2024-02-22 01:35:18 +0100

    Delete src/main/java/Controllers/back/UserDetailUpdater.java

--- src/main/java/Controllers/back/UserDetailUpdater.java
+++
@@ -1,9 +0,0 @@
-package Controllers.back;
-
-import Models.Users;
-
-public interface UserDetailUpdater {
-    void updateDetails(Users user); // called when a user item is selected , gets the details and displays them
-    void refreshUserList();
-    void clearForm();
-}
