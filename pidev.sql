4d72ac82cc79ae201517f2dc9f1e3ef53364ee4b
Hamzachaiebgit <100535374+Hamzachaiebgit@users.noreply.github.com>
2024-02-26 03:46:06 +0100

    Add files via upload

--- pidev.sql
+++ pidev.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 24, 2024 at 01:01 AM
+-- Generation Time: Feb 26, 2024 at 03:43 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -42,8 +42,9 @@ CREATE TABLE `feedback` (
 --
 
 INSERT INTO `feedback` (`feedback_id`, `status`, `type`, `question`, `answer`, `user_satisfaction`, `id_U`) VALUES
-(2, 'Closed', 'General Feedback', 'i love the application!', 'thank you , we appreciate it , happy to help', NULL, 12),
-(3, 'Closed', 'Bug Report', 'i had an error buying art , thank you !', 'we\'ll try to resolve that within 24 hours', '', 12);
+(2, 'Closed', 'General Feedback', 'i love the application!', 'thank you!', NULL, 12),
+(3, 'Closed', 'Bug Report', 'i had an error buying art , thank you !', 'we\'ll try to resolve that within 24 hours', '', 12),
+(4, 'Closed', 'Feature Request', 'hello!', 'hi!', '', 27);
 
 -- --------------------------------------------------------
 
@@ -68,11 +69,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`user_id`, `firstname`, `lastname`, `password`, `email_address`, `role`, `account_status`, `date_created`, `last_login`) VALUES
-(2, 'example', 'nothing', 'securepassword123', 'hamza.chaieb@example.com', 'User', 'Disabled', '2024-02-17 01:37:38', NULL),
-(12, 'hamzamod', 'chaieb', '123', 'hamza.chaieb@esprit.tn', 'Amateur', 'Active', '2024-02-17 19:06:35', '2024-02-23 01:44:25'),
-(15, 'modified', 'chaieb', '123', 'hamza.chaieb1@esprit.tn', 'Amateur', 'Disabled', '2024-02-17 19:06:35', '2024-02-18 23:58:52'),
-(22, 'Admin', 'UserAdmin', '1234', 'Admin@admin.com', 'UserAdmin', 'Active', '2024-02-17 01:37:38', '2024-02-24 00:49:41'),
-(27, 'addedbysuser', 'addedbysuser', '123', 'addedbysuser@esprit.tn', 'Artist', 'Active', '2024-02-24 00:17:24', NULL);
+(12, 'hamzamodon', 'chaieb', '$2a$10$/2.uK.z5S.W28llhUw8sxOUgYt7UuMkIc6Q/.rD6qhB8L.rsvmDem', 'hamza.chaieb@esprit.tn', 'Amateur', 'Active', '2024-02-17 19:06:35', '2024-02-26 02:21:46'),
+(15, 'modified', 'chaieb', '$2a$10$4W0X2Yl5EIro2tM1rhq4Pe4V6RT6/8681K/jz4c.p2hJqaGaXlzbG', 'hamza.chaieb1@esprit.tn', 'Amateur', 'Disabled', '2024-02-17 19:06:35', '2024-02-18 23:58:52'),
+(22, 'Admin', 'UserAdmin', '$2a$10$emmlw7tGQSVzjZ2aiyALDOFDXvHMMz61UgMAukOJ4jo5OxVS5sHIK', 'Admin@admin.com', 'UserAdmin', 'Active', '2024-02-17 01:37:38', '2024-02-26 02:20:30'),
+(27, 'addedbysuser', 'addedbysuser', '$2a$10$XrbWaTwuUiaeMGvZNxB8vO0fjl3OD2YDN0vRMjSbrRd1VgQhxKfP.', 'addedbysuser@esprit.tn', 'Artist', 'Active', '2024-02-24 00:17:24', '2024-02-25 01:46:02'),
+(28, 'testing', 'create', '$2a$10$76crDU9VRMATTRHAbfOYc.r8aThMhk5xTN9afZBA9J/tVIBviTk1O', 'creat@esprit.tn', 'Amateur', 'Active', '2024-02-25 00:46:59', NULL),
+(29, 'tryinghash', 'hash', '$2a$10$Pd4V5sppG65S3zUuFZiapuiEXUfe4w6PEn/WpdhMcdJLwG2/861lC', 'hash@esprit.tn', 'Artist', 'Active', '2024-02-25 03:00:52', '2024-02-25 03:29:43');
 
 --
 -- Indexes for dumped tables
@@ -100,13 +102,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- Constraints for dumped tables
